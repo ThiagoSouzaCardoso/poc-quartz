5df783abdb080af975b234b8801aa252112de7de
Thiago <thiagocardosok@gmail.com>
2019-06-02 11:22:42 -0300

    init commit

---
+++ src/main/java/br/com/demo/spring/batch/pocquartz/PocQuartzApplication.java
@@ -0,0 +1,13 @@
+package br.com.demo.spring.batch.pocquartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocQuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocQuartzApplication.class, args);
+    }
+
+}
